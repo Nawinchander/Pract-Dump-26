45de8b7ca496b678aa0f990226f674f3a9bde9fe
Nawin chander <nawinnapzy289@gmail.com>
2026-06-05 21:40:07 +0530

    add

---
+++ Script.js
@@ -0,0 +1,28 @@
+var abc = 1234
+console.log(abc)
+
+let a1 = () =>{
+
+}
+
+setTimeout(() => {
+    
+}, 3000);
+setInterval();
+
+let ob = {
+    'name' : 'Vicky',
+    'std' : 12
+}
+
+var ob1 = {
+    'nam' : 'vic',
+    
+    func = () =>{
+
+    }
+}
+
+function abc (){
+    
+}
