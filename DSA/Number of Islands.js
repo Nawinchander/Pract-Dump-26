d22a742db997787ef845360ae1497e18b94c1f49
Nawin chander <nawinnapzy289@gmail.com>
2026-06-08 20:53:06 +0530

    add

---
+++ DSA/Number of Islands.js
@@ -0,0 +1,15 @@
+// Number of Islands — 2 FAANG-Level Problems
+
+// Grid Traversal
+// DFS
+// BFS
+// Union Find
+// Connected Components
+
+
+// Problem 1: Number of Islands
+// LeetCode 200
+
+// Given a 2D grid of '1' (land) and '0' (water), return the number of islands.
+
+
