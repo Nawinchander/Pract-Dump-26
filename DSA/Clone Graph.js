d589af68a3579cf8a07540343cb6a25813d6f701
Nawin chander <nawinnapzy289@gmail.com>
2026-06-08 20:57:33 +0530

    ad

--- DSA/Clone Graph.js
+++ DSA/Clone Graph.js
@@ -16,3 +16,38 @@ Example
 |     |
 4 --- 3
 
+class Node {
+    constructor(val, neighbors = []) {
+        this.val = val;
+        this.neighbors = neighbors;
+    }
+}
+
+function cloneGraph(node) {
+    if (!node) return null;
+
+    const map = new Map();
+
+    function dfs(curr) {
+        if (map.has(curr)) {
+            return map.get(curr);
+        }
+
+        const clone = new Node(curr.val);
+
+        map.set(curr, clone);
+
+        for (const neighbor of curr.neighbors) {
+            clone.neighbors.push(
+                dfs(neighbor)
+            );
+        }
+
+        return clone;
+    }
+
+    return dfs(node);
+}
+
+
+
