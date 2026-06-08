e86b4768cd110dc571615a5cf819e3e1a6c05d0f
Nawin chander <nawinnapzy289@gmail.com>
2026-06-08 21:34:24 +0530

    add

--- DSA/Clone Graph.js
+++ DSA/Clone Graph.js
@@ -76,4 +76,49 @@ Each node contains:
 11.random -> 13
 
 
+class Node {
+    constructor(val, next = null, random = null) {
+        this.val = val;
+        this.next = next;
+        this.random = random;
+    }
+}
+
+function copyRandomList(head) {
+    if (!head) return null;
+
+    const map = new Map();
+
+    let curr = head;
+
+    while (curr) {
+        map.set(
+            curr,
+            new Node(curr.val)
+        );
+        curr = curr.next;
+    }
+
+    curr = head;
+
+    while (curr) {
+        map.get(curr).next =
+            curr.next
+                ? map.get(curr.next)
+                : null;
+
+        map.get(curr).random =
+            curr.random
+                ? map.get(curr.random)
+                : null;
+
+        curr = curr.next;
+    }
+
+    return map.get(head);
+}
+
+Time: O(n)
+Space: O(n)
+
 
