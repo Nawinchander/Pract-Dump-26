246ae995e65109e8b209ec73baecbb581bafc367
Nawin chander <nawinnapzy289@gmail.com>
2026-06-09 21:59:26 +0530

    add

--- DSA/Word Ladder.js
+++ DSA/Word Ladder.js
@@ -28,6 +28,57 @@ Output:
 
 // hit → hot → dot → dog → cog
 
+function ladderLength(
+    beginWord,
+    endWord,
+    wordList
+) {
+    const wordSet = new Set(wordList);
+
+    if (!wordSet.has(endWord))
+        return 0;
+
+    const queue = [[beginWord, 1]];
+
+    while (queue.length) {
+        const [word, steps] =
+            queue.shift();
+
+        if (word === endWord)
+            return steps;
+
+        for (let i = 0; i < word.length; i++) {
+
+            for (
+                let c = 97;
+                c <= 122;
+                c++
+            ) {
+
+                const newWord =
+                    word.slice(0, i) +
+                    String.fromCharCode(c) +
+                    word.slice(i + 1);
+
+                if (wordSet.has(newWord)) {
+
+                    queue.push([
+                        newWord,
+                        steps + 1
+                    ]);
+
+                    wordSet.delete(
+                        newWord
+                    );
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+
 
 
 
