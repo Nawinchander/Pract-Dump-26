755ce082a84024b544682e5e3fb3e2ba12c491e2
Nawin chander <nawinnapzy289@gmail.com>
2026-06-09 21:59:06 +0530

    add

--- DSA/Word Ladder.js
+++ DSA/Word Ladder.js
@@ -5,14 +5,33 @@
 // Bi-directional BFS
 
 
-Word Ladder
-LeetCode 127 (Hard)
+// Word Ladder
+// LeetCode 127 (Hard)
 
-Transform beginWord into endWord.
+// Transform beginWord into endWord.
 
-Only one character may change at a time.
+// Only one character may change at a time.
 
-Every intermediate word must exist in the dictionary.
+// Every intermediate word must exist in the dictionary.
 
-Return the minimum number of transformations.
+// Return the minimum number of transformations.
+
+beginWord = "hit"
+
+endWord = "cog"
+
+wordList =
+["hot","dot","dog","lot","log","cog"]
+
+Output:
+5
+
+// hit → hot → dot → dog → cog
+
+
+
+
+
+
+
 
