6c5bb94fec7e835bb4ed79dd950f8b9c5456e423
Nawin chander <nawinnapzy289@gmail.com>
2026-06-11 02:54:13 +0530

    add

--- DSA/Trapping Rain Water.js
+++ DSA/Trapping Rain Water.js
@@ -1,1 +1,17 @@
 // rain water
+
+Two Pointers
+Monotonic Stack
+Prefix Arrays
+Greedy
+
+// Problem 3: Trapping Rain Water
+// LeetCode 42 (Hard)
+
+// Given elevation heights, compute how much rainwater can be trapped.
+
+
+
+
+
+
