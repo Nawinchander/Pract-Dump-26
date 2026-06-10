842cc25b6cb0167d95b4a312fedb4af69dcee791
Nawin chander <nawinnapzy289@gmail.com>
2026-06-10 06:06:19 +0530

    add

--- DSA/Deserialize Binary.js
+++ DSA/Deserialize Binary.js
@@ -1,20 +1,31 @@
-Serialize / Deserialize Binary Tree — 2 FAANG-Level Problems
+// Serialize / Deserialize Binary Tree — 2 FAANG-Level Problems
 
-These problems test:
+// These problems test:
 
-Trees
-DFS
-BFS
-Recursion
-String Encoding
-Tree Reconstruction
+// Trees
+// DFS
+// BFS
+// Recursion
+// String Encoding
+// Tree Reconstruction
 
 
 
-Problem 1: Serialize and Deserialize Binary Tree
-LeetCode 297 (Hard)
+// Problem 1: Serialize and Deserialize Binary Tree
+// LeetCode 297 (Hard)
 
-Design an algorithm to convert a binary tree into a string and reconstruct it back.
+// Design an algorithm to convert a binary tree into a string and reconstruct it back.
+
+        1
+       / \
+      2   3
+         / \
+        4   5
+
+
+"1,2,null,null,3,4,null,null,5,null,null"
+
+
 
 
 
