9a206f0e207fd93770c1b5c8ac5177977934ccad
Nawin chander <nawinnapzy289@gmail.com>
2026-08-28 20:42:27 +0530

    ad

---
+++ DSA/26/Alien Dictionary/ad.js
@@ -0,0 +1,29 @@
+// Given:
+
+// [
+//     "wrt",
+//     "wrf",
+//     "er",
+//     "ett",
+//     "rftt"
+// ]
+
+// Determine the ordering of characters.
+
+// Answer:
+
+// "wertf"
+// Core observation
+
+// Compare adjacent words.
+
+// wrt
+// wrf
+
+// First difference:
+
+
+
+
+
+
