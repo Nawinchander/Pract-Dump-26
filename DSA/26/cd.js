9b90b5a930d3bf641185abb0fd93d820719337d7
Nawin chander <nawinnapzy289@gmail.com>
2026-08-28 20:25:25 +0530

    ad

---
+++ DSA/26/cd.js
@@ -0,0 +1,40 @@
+// Return all shortest transformation sequences.
+
+// A transformation changes exactly one character at a time.
+
+// Expected:
+
+// [
+//   ["hit","hot","dot","dog","cog"],
+//   ["hit","hot","lot","log","cog"]
+// ]
+// Why this is difficult
+
+// A normal Word Ladder is just BFS.
+
+// Here we need:
+
+// BFS
+//  ↓
+// find shortest distance
+//  ↓
+// preserve ALL shortest paths
+//  ↓
+// DFS/backtracking
+
+// Return all shortest transformation sequences.
+
+// A transformation changes exactly one character at a time.
+
+// Expected:
+
+// [
+//   ["hit","hot","dot","dog","cog"],
+//   ["hit","hot","lot","log","cog"]
+// ]
+
+
+
+
+
+
