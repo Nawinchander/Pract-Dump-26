24fc0fdcc6015747076af97939f07566a22e0c70
Nawin chander <nawinnapzy289@gmail.com>
2026-08-29 20:17:05 +0530

    ad

--- DSA/26/Alien Dictionary/Sliding Window Median.js
+++ DSA/26/Alien Dictionary/Sliding Window Median.js
@@ -34,6 +34,97 @@
 //     (top(left) + top(right)) / 2
 
 
+class Heap {
+    constructor(compare) {
+        this.data = [];
+        this.compare = compare;
+    }
+
+    push(value) {
+        this.data.push(value);
+        this.up(this.data.length - 1);
+    }
+
+    pop() {
+        if (this.data.length === 1) {
+            return this.data.pop();
+        }
+
+        const root = this.data[0];
+
+        this.data[0] =
+            this.data.pop();
+
+        this.down(0);
+
+        return root;
+    }
+
+    peek() {
+        return this.data[0];
+    }
+
+    get size() {
+        return this.data.length;
+    }
+
+    up(index) {
+        while (index > 0) {
+            const parent = Math.floor((index - 1) / 2);
+
+            if (
+                this.compare(
+                    this.data[parent],
+                    this.data[index]
+                )
+            ) break;
+
+            [this.data[parent], this.data[index]] =
+                [this.data[index], this.data[parent]];
+
+            index = parent;
+        }
+    }
+
+    down(index) {
+        while (true) {
+            let best = index;
+
+            const left = index * 2 + 1;
+            const right = index * 2 + 2;
+
+            if (
+                left < this.data.length &&
+                !this.compare(
+                    this.data[best],
+                    this.data[left]
+                )
+            ) {
+                best = left;
+            }
+
+            if (
+                right < this.data.length &&
+                !this.compare(
+                    this.data[best],
+                    this.data[right]
+                )
+            ) {
+                best = right;
+            }
+
+            if (best === index) break;
+
+            [this.data[index], this.data[best]] =
+                [this.data[best], this.data[index]];
+
+            index = best;
+        }
+    }
+}
+
+
+
 
 
 
