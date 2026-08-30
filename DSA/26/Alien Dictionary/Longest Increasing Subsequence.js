fc00cb053223636929fe22e47a1287a907d938fe
Nawin chander <nawinnapzy289@gmail.com>
2026-08-30 18:58:00 +0530

    ad

---
+++ DSA/26/Alien Dictionary/Longest Increasing Subsequence.js
@@ -0,0 +1,23 @@
+// Longest Increasing Subsequence — O(n log n)
+
+// Input:
+
+// [10,9,2,5,3,7,101,18]
+
+// Answer:
+
+// 4
+
+// Example:
+
+// 2 → 3 → 7 → 101
+// Important insight
+
+// Don't store the actual subsequence.
+
+// Maintain
+
+
+
+
+
