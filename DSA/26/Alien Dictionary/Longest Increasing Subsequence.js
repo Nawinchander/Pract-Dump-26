1a4e3a6cdb99a7ac5ea2b7ad3cdfb4d9d347b755
Nawin chander <nawinnapzy289@gmail.com>
2026-08-30 18:58:40 +0530

    ad

--- DSA/26/Alien Dictionary/Longest Increasing Subsequence.js
+++ DSA/26/Alien Dictionary/Longest Increasing Subsequence.js
@@ -17,6 +17,39 @@
 
 // Maintain
 
+// tails[i] =
+// smallest possible ending value
+// of an increasing subsequence of length i + 1
+
+
+
+function lengthOfLIS(nums) {
+    const tails = [];
+
+    for (const num of nums) {
+        let left = 0;
+        let right = tails.length;
+
+        while (left < right) {
+            const mid = Math.floor(
+                (left + right) / 2
+            );
+
+            if (tails[mid] < num) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        tails[left] = num;
+    }
+
+    return tails.length;
+}
+
+
+
 
 
 
