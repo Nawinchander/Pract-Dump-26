7e09ecf5017111a0476f7d1e22c4d93d52cbecc4
Nawin chander <nawinnapzy289@gmail.com>
2026-08-30 19:00:46 +0530

    ad

---
+++ DSA/26/Alien Dictionary/Edit Distance.js
@@ -0,0 +1,24 @@
+// Convert:
+
+// "horse"
+
+// into:
+
+// "ros"
+
+// Operations:
+
+// Insert
+// Delete
+// Replace
+
+// Answer: 3 DP State
+
+
+// Define: dp[i][j]
+
+// as: minimum operations required to convert first i characters of word1 into first j characters of word2.
+
+
+
+
