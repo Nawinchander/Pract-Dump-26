b73b49b416b9cb62e3f78f8a4238af67ce6ac11d
Nawin chander <nawinnapzy289@gmail.com>
2026-08-31 18:55:17 +0530

    ad

--- DSA/26/Alien Dictionary/Number of Islands.js
+++ DSA/26/Alien Dictionary/Number of Islands.js
@@ -26,5 +26,51 @@
 
 // and decrement island count.
 
+class DSU {
+    constructor(n) {
+        this.parent = Array.from(
+            { length: n },
+            (_, i) => i
+        );
+
+        this.rank = new Array(n).fill(0);
+    }
+
+    find(x) {
+        if (this.parent[x] !== x) {
+            this.parent[x] =
+                this.find(this.parent[x]);
+        }
+
+        return this.parent[x];
+    }
+
+    union(a, b) {
+        let rootA = this.find(a);
+        let rootB = this.find(b);
+
+        if (rootA === rootB) {
+            return false;
+        }
+
+        if (this.rank[rootA] < this.rank[rootB]) {
+            [rootA, rootB] = [rootB, rootA];
+        }
+
+        this.parent[rootB] = rootA;
+
+        if (
+            this.rank[rootA] ===
+            this.rank[rootB]
+        ) {
+            this.rank[rootA]++;
+        }
+
+        return true;
+    }
+}
 
 
+
+
+/// Main algorithm :::;
