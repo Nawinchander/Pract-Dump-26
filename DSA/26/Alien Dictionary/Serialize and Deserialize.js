7c633f2589bef60bc3071e6467eb54c7202356f2
Nawin chander <nawinnapzy289@gmail.com>
2026-08-31 18:49:33 +0530

    ad

--- DSA/26/Alien Dictionary/Serialize and Deserialize.js
+++ DSA/26/Alien Dictionary/Serialize and Deserialize.js
@@ -14,6 +14,55 @@
 
 
 
+function serialize(root) {
+    const result = [];
+
+    function dfs(node) {
+        if (!node) {
+            result.push("null");
+            return;
+        }
+
+        result.push(String(node.val));
+
+        dfs(node.left);
+        dfs(node.right);
+    }
+
+    dfs(root);
+
+    return result.join(",");
+}
+
+
+function deserialize(data) {
+    const values = data.split(",");
+    let index = 0;
+
+    function dfs() {
+        if (values[index] === "null") {
+            index++;
+            return null;
+        }
+
+        const node = {
+            val: Number(values[index]),
+            left: null,
+            right: null
+        };
+
+        index++;
+
+        node.left = dfs();
+        node.right = dfs();
+
+        return node;
+    }
+
+    return dfs();
+}
+
+
 
 
 
