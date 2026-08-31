266733f99e7065db768215a85b729ea932ebc0fa
Nawin chander <nawinnapzy289@gmail.com>
2026-08-31 18:48:53 +0530

    ad

---
+++ DSA/26/Alien Dictionary/Serialize and Deserialize.js
@@ -0,0 +1,22 @@
+// Serialize and Deserialize Binary Tree
+
+// This is a very good tree + recursion + system thinking question.
+
+// Tree:
+
+//        1
+//       / \
+//      2   3
+//         / \
+//        4   5
+
+// Serialize: "1,2,null,null,3,4,null,null,5,null,null"
+
+
+
+
+
+
+
+
+
