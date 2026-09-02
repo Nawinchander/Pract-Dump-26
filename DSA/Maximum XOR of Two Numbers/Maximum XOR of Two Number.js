e866a9cfb9f4e1f625d2d32b8e528ec1eae603b6
Nawin chander <nawinnapzy289@gmail.com>
2026-09-02 21:24:41 +0530

    ad

---
+++ DSA/Maximum XOR of Two Numbers/Maximum XOR of Two Number.js
@@ -0,0 +1,30 @@
+// Binary Trie
+
+// Each node has:
+
+{
+    zero: null,
+    one: null
+}
+
+// Numbers are inserted bit by bit.
+
+// For example:
+
+// 25 = 11001
+
+// becomes:
+
+// root
+//  |
+//  1
+//  |
+//  1
+//  |
+//  0
+//  |
+//  0
+//  |
+//  1
+
+
