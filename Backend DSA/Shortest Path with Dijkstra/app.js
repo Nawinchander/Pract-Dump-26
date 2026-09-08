044852c457af4581618bccbc90f7bd0ad144905b
Nawin chander <nawinnapzy289@gmail.com>
2026-09-08 13:37:59 +0530

    ad

---
+++ Backend DSA/Shortest Path with Dijkstra/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const pathRoutes =
+  require("./routes/path.routes");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/dsa", pathRoutes);
+
+module.exports = app;
+
+
