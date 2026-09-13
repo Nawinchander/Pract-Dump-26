6cda12b5c32cdc9cc82bf244cc8a4e9a2dd0ecc9
Nawin chander <nawinnapzy289@gmail.com>
2026-09-13 09:34:10 +0530

    ad

--- Adv Backend DSA/src/models/mongo/TrafficSnapshot.js
+++ Adv Backend DSA/src/models/mongo/TrafficSnapshot.js
@@ -1,3 +1,38 @@
+const mongoose = require("mongoose");
 
+const trafficSnapshotSchema =
+    new mongoose.Schema({
+        source: {
+            type: String,
+            required: true
+        },
+
+        destination: {
+            type: String,
+            required: true
+        },
+
+        trafficMultiplier: {
+            type: Number,
+            required: true,
+            min: 0.1
+        },
+
+        capturedAt: {
+            type: Date,
+            default: Date.now
+        }
+    });
+
+module.exports =
+    mongoose.model(
+        "TrafficSnapshot",
+        trafficSnapshotSchema
+    );
+
+
+
+
+    
 
 
