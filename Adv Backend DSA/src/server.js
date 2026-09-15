e3b3afe50a3302c1e781966626515afd1f01aa27
Nawin chander <nawinnapzy289@gmail.com>
2026-09-15 15:36:13 +0530

    ad

---
+++ Adv Backend DSA/src/server.js
@@ -0,0 +1,27 @@
+
+
+require("dotenv").config();
+
+const app = require("./app");
+const connectMongo =
+    require("./config/mongo");
+
+const PORT =
+    process.env.PORT || 5000;
+
+async function startServer() {
+
+    await connectMongo();
+
+    app.listen(PORT, () => {
+
+        console.log(
+            `Server running on port ${PORT}`
+        );
+
+    });
+}
+
+startServer();
+
+
