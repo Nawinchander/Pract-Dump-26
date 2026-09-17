a8d3a88bfacdda29339e16d3fbd9b554b74c5a7b
Nawin chander <nawinnapzy289@gmail.com>
2026-09-17 19:04:32 +0530

    ad

--- Adv Backend DSA/src/tests/api/route.api.test.js
+++ Adv Backend DSA/src/tests/api/route.api.test.js
@@ -1,3 +1,77 @@
 
+const request =
+    require("supertest");
+
+const app =
+    require("../../src/app");
+
+describe("Route API", () => {
+
+    test(
+        "POST /api/routes/shortest",
+        async () => {
+
+            const response =
+                await request(app)
+                    .post(
+                        "/api/routes/shortest"
+                    )
+                    .send({
+                        source: "A",
+                        destination: "D",
+
+                        edges: [
+                            {
+                                source: "A",
+                                destination: "B",
+                                weight: 4
+                            },
+                            {
+                                source: "A",
+                                destination: "C",
+                                weight: 2
+                            },
+                            {
+                                source: "C",
+                                destination: "B",
+                                weight: 1
+                            },
+                            {
+                                source: "B",
+                                destination: "D",
+                                weight: 5
+                            }
+                        ]
+                    });
+
+            expect(response.statusCode)
+                .toBe(200);
+
+            expect(
+                response.body.success
+            ).toBe(true);
+
+            expect(
+                response.body.data.distance
+            ).toBe(8);
+        }
+    );
+
+});
+
+
+
+
+//  npm run test:api
+
+//  35 api error testing
+
+/* 
+
+fuckk off you alll 
+
+*/
+
+
 
 
