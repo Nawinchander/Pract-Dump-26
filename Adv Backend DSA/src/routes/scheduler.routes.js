5696843fea8f0809b2269872457ed10c1b525a5c
Nawin chander <nawinnapzy289@gmail.com>
2026-09-22 14:02:57 +0530

    ad

--- Adv Backend DSA/src/routes/scheduler.routes.js
+++ Adv Backend DSA/src/routes/scheduler.routes.js
@@ -1,14 +1,39 @@
+// const express = require("express");
+
+// const {
+//     schedule
+// } = require("../controllers/scheduler.controller");
+
+// const router = express.Router();
+
+// router.post(
+//     "/critical-path",
+//     schedule
+// );
+
+// module.exports = router;
+
+
+
 const express = require("express");
 
 const {
-    schedule
-} = require("../controllers/scheduler.controller");
+    schedule,
+    dependencyImpact
+} = require(
+    "../controllers/scheduler.controller"
+);
 
 const router = express.Router();
 
 router.post(
     "/critical-path",
     schedule
+);
+
+router.post(
+    "/dependency-impact",
+    dependencyImpact
 );
 
 module.exports = router;
